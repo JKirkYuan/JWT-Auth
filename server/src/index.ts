3d2b3585e5ec803e0acd37b86e9a9a272d9e937b
JKirkYuan <yuanjt810@gmail.com>
2021-01-14 14:03:03 -0700

    test

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,6 +14,7 @@ import { createTypeormConn } from './utils/createTypeormConn';
 
 (async () => {
   const app = express();
+  app.set('trust proxy', 1);
   console.log(process.env.NODE_ENV);
   console.log(process.env.ORIGIN);
   app.use(
